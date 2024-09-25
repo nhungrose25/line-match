49199aa839f61ff8c6c627233767c8d7bfe0685e
nhungrose25 <160495719+nhungrose25@users.noreply.github.com>
2024-09-25 21:15:48 +0700

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,160 @@
+// This list of lists represents the state of the board
+export let table = [
+	["Empty", "Empty", "Empty"],
+	["Empty", "Empty", "Empty"],
+	["Empty", "Empty", "Empty"]
+];
+
+export function resetTable()
+{
+	// Reset the table to Empty
+	table = [
+		["Empty", "Empty", "Empty"],
+		["Empty", "Empty", "Empty"],
+		["Empty", "Empty", "Empty"]
+	];
+}
+
+export function getEmpty(t)
+{
+	// Return empty spaces indices
+	const emptyIndices = [];
+	for (let x = 0; x < 3; x++)
+		for (let y = 0; y < 3; y++)
+			if (t[x][y] == "Empty")
+				emptyIndices.push({x: x, y: y});
+	return emptyIndices;
+}
+
+export function checkGameOver(t)
+{
+	// Check if a player has won the match
+	if (t[0][0] == t[0][1] && t[0][1] == t[0][2] && t[0][0] != "Empty")
+		return { player: t[0][0], spaces: [[0, 0], [0, 1], [0, 2]] };
+	else if (t[1][0] == t[1][1] && t[1][1] == t[1][2] && t[1][0] != "Empty")
+		return { player: t[1][0], spaces: [[1, 0], [1, 1], [1, 2]] };
+	else if (t[2][0] == t[2][1] && t[2][1] == t[2][2] && t[2][0] != "Empty")
+		return { player: t[2][0], spaces: [[2, 0], [2, 1], [2, 2]] };
+	else if (t[0][0] == t[1][0] && t[1][0] == t[2][0] && t[0][0] != "Empty")
+		return { player: t[0][0], spaces: [[0, 0], [1, 0], [2, 0]] };
+	else if (t[0][1] == t[1][1] && t[1][1] == t[2][1] && t[0][1] != "Empty")
+		return { player: t[0][1], spaces: [[0, 1], [1, 1], [2, 1]] };
+	else if (t[0][2] == t[1][2] && t[1][2] == t[2][2] && t[0][2] != "Empty")
+		return { player: t[0][2], spaces: [[0, 2], [1, 2], [2, 2]] };
+	else if (t[0][0] == t[1][1] && t[1][1] == t[2][2] && t[0][0] != "Empty")
+		return { player: t[0][0], spaces: [[0, 0], [1, 1], [2, 2]] };
+	else if (t[0][2] == t[1][1] && t[1][1] == t[2][0] && t[0][2] != "Empty")
+		return { player: t[0][2], spaces: [[0, 2], [1, 1], [2, 0]] };
+	// Check for a draw
+	else if (getEmpty(t).length == 0)
+		return { player: "Draw", spaces: null};
+	// Nothing special going on
+	else
+		return { player: "Nothing", spaces: null};
+}
+
+export function minimax(t, player, a, b, depth)
+{
+	// Minimax algorithm with Alpha-Beta pruning
+	
+	// Check if game is over. Depth is an incentive to win as fast as possible
+	const whoWon = checkGameOver(t).player;
+	if      (whoWon == "Nought") return { score: -10 + depth, bestMove: null };
+	else if (whoWon == "Cross" ) return { score:  10 - depth, bestMove: null };
+	else if (whoWon == "Draw"  ) return { score:   0        , bestMove: null };
+	
+	// Shuffled possible moves. NOTE: not the best way to shuffle, but sufficient
+	const moveList = getEmpty(t).sort(() => 0.5 - Math.random());
+	
+	let bestMove;
+	let bestScore;
+	
+	// Crosses try to maximize the score
+	if (player == "Cross") {
+		bestScore = -Infinity;
+		for (const move of moveList) {
+			t[move.x][move.y] = "Cross"; // Do the move
+			const score = minimax(t, "Nought", a, b, depth + 1).score;
+			t[move.x][move.y] = "Empty"; // Undo the move
+			if (score > bestScore) {
+				bestScore = score; // Save best score
+				bestMove = move;   // Save best move
+			}
+			// Pruning
+			a = Math.max(a, score);
+			if (a >= b) break;
+		}
+		return {score: bestScore, bestMove: bestMove};
+		
+	// Noughts try to minimize the score
+	} else {
+		bestScore = Infinity;
+		for (const move of moveList) {
+			t[move.x][move.y] = "Nought"; // Do the move
+			const score = minimax(t, "Cross", a, b, depth + 1).score;
+			t[move.x][move.y] = "Empty"; // Undo the move
+			if (score < bestScore) {
+				bestScore = score; // Save best score
+				bestMove = move;   // Save best move
+			}
+			// Pruning
+			b = Math.min(b, score);
+			if (b <= a) break;
+		}
+		return {score: bestScore, bestMove: bestMove};
+	}
+}
+
+
+const scriptsInEvents = {
+
+	async Gameevents_Event8_Act1(runtime, localVars)
+	{
+		// Get current game situation
+		const result = checkGameOver(table);
+		
+		// Check if the game is over and return the result
+		switch (result.player) {
+			case "Nothing":
+				runtime.setReturnValue("Normal");
+				break;
+			case "Draw":
+				runtime.setReturnValue("Draw");
+				break;
+			default:
+				localVars.winner0 = result.spaces[0][0] + "|" + result.spaces[0][1];
+				localVars.winner1 = result.spaces[1][0] + "|" + result.spaces[1][1];
+				localVars.winner2 = result.spaces[2][0] + "|" + result.spaces[2][1];
+				runtime.setReturnValue("Win");
+		}
+	},
+
+	async Gameevents_Event12_Act2(runtime, localVars)
+	{
+		resetTable();
+	},
+
+	async Gameevents_Event14_Act4(runtime, localVars)
+	{
+		const space = runtime.objects.Space.getFirstPickedInstance();
+		table[space.instVars.xPos][space.instVars.yPos] = runtime.globalVars.currentPlayer;
+	},
+
+	async Gameevents_Event22_Act2(runtime, localVars)
+	{
+		const result = minimax(table, runtime.globalVars.currentPlayer, -Infinity, Infinity, 0);
+		localVars.aiX = result.bestMove.x;
+		localVars.aiY = result.bestMove.y;
+		table[localVars.aiX][localVars.aiY] = runtime.globalVars.currentPlayer;
+	},
+
+	async Gameevents_Event24_Act2(runtime, localVars)
+	{
+		const space = runtime.objects.Space.getFirstPickedInstance();
+		table[space.instVars.xPos][space.instVars.yPos] = runtime.globalVars.currentPlayer;
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
